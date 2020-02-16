6c567d037d5cd9577b98dea8acb7dda6c4eb1b58
Addikins <addikinsplays@gmail.com>
2020-02-16 16:37:31 -0700

    Add Player & Camera controllers

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 3f;
+    [SerializeField] Transform Target, Player;
+    [SerializeField] float distanceFromTarget = 2f;
+
+    float mouseX, mouseY;
+
+    void Start()
+    {
+    //     Cursor.visible = false;
+    //     Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate()
+    {
+        CameraMovement();
+    }
+
+    void CameraMovement()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        Vector3 targetRotation = new Vector3(mouseY, mouseX);
+        transform.eulerAngles = targetRotation;
+
+
+        // transform.LookAt(Target);
+
+        // Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        // Player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+
+
+}
---
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(horizontalInput, 0f, verticalInput).normalized * speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
