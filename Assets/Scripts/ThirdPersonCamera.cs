0a71e0f957cc6e4569e453ed0cffec919e59022a
Addikins <addikinsplays@gmail.com>
2020-02-17 00:17:17 -0700

    Fix camera and player controller config
    Add Synty Adventure pack
    Preset animator

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -2,40 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonCamera : MonoBehaviour
-{
+public class ThirdPersonCamera : MonoBehaviour {
     [SerializeField] float rotationSpeed = 3f;
-    [SerializeField] Transform target;
+    [SerializeField] float scrollSpeed = 6f;
+    [SerializeField] Transform target = null;
     [SerializeField] float distanceFromTarget = 2f;
-    [SerializeField] Vector2 verticalMinMax = new Vector2(-35, 85);
+    [SerializeField] Vector2 distanceMinMax = new Vector2 (2f, 15f);
+    [SerializeField] Vector2 verticalMinMax = new Vector2 (-5, 85);
     [SerializeField] float rotationSmoothTime = .12f;
 
     Vector3 rotationSmoothVelocity;
     Vector3 currentRotation;
 
-    float mouseX, mouseY;
+    float mouseX, mouseY, mouseScroll;
 
-    void Start()
-    {
+    void Start () {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void LateUpdate()
-    {
-        CameraMovement();
+    private void LateUpdate () {
+        CameraMovement ();
     }
 
-    void CameraMovement()
-    {
-        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
-        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
-        mouseY = Mathf.Clamp(mouseY, verticalMinMax.x, verticalMinMax.y);
+    void CameraMovement () {
+        mouseScroll -= Input.GetAxis ("Mouse ScrollWheel") * scrollSpeed;
+        mouseX += Input.GetAxis ("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis ("Mouse Y") * rotationSpeed;
+        mouseScroll = Mathf.Clamp (mouseScroll, distanceMinMax.x, distanceMinMax.y);
+        mouseY = Mathf.Clamp (mouseY, verticalMinMax.x, verticalMinMax.y);
 
-        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(mouseY, mouseX), ref rotationSmoothVelocity, rotationSmoothTime);
-
-
+        currentRotation = Vector3.SmoothDamp (currentRotation, new Vector3 (mouseY, mouseX), ref rotationSmoothVelocity, rotationSmoothTime);
         transform.eulerAngles = currentRotation;
+
+        distanceFromTarget = mouseScroll;
 
         transform.position = target.position - transform.forward * distanceFromTarget;
     }
