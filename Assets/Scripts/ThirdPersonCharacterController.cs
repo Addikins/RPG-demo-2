30f01344dac2762e07970dc038a77b156b091df1
Addikins <addikinsplays@gmail.com>
2020-02-19 12:54:52 -0700

    Add falling animation and raycasting

--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -2,124 +2,152 @@
 using System.Collections;
 using UnityEngine;
 
-public class ThirdPersonCharacterController : MonoBehaviour {
+namespace RPG.Control {
 
-    [SerializeField] float walkSpeed = 2f;
-    [SerializeField] float runSpeed = 6f;
-    [SerializeField] float rollSpeed = 2f;
-    [SerializeField] float rollTime = 2.4f;
-    [SerializeField] float rollDelay = .5f;
-    [SerializeField] float maxRollTime = 1.5f;
-    [SerializeField] float speedSmoothTime = 0.1f;
-    [SerializeField] float turnSmoothTime = 0.2f;
-    [SerializeField] float walkingAnimationSpeed = .5f;
-    [SerializeField] float runningAnimationSpeed = 1f;
-    [SerializeField] float rollingAnimationSpeed = 1f;
-    [SerializeField] float maxAnimationSpeed = 1.5f;
-    [SerializeField] float bonusSpeedMultiplier = 1.5f;
-    [SerializeField] bool hasSpeedBonus = true;
-    private State state;
-    private float timeSpentRolling;
+    public class ThirdPersonCharacterController : MonoBehaviour {
 
-    private enum State {
-        Normal,
-        Rolling,
-        Falling,
-    }
+        [SerializeField] float walkSpeed = 2f;
+        [SerializeField] float runSpeed = 6f;
+        [SerializeField] float rollSpeed = 2f;
+        [SerializeField] float rollTime = 2.4f;
+        [SerializeField] float rollDelay = .5f;
+        [SerializeField] float maxRollTime = 1.5f;
+        [SerializeField] float speedSmoothTime = 0.1f;
+        [SerializeField] float turnSmoothTime = 0.2f;
+        [SerializeField] float walkingAnimationSpeed = .5f;
+        [SerializeField] float runningAnimationSpeed = 1f;
+        [SerializeField] float rollingAnimationSpeed = 1f;
+        [SerializeField] float maxAnimationSpeed = 1.5f;
+        [SerializeField] float bonusSpeedMultiplier = 1.5f;
+        [SerializeField] bool hasSpeedBonus = true;
+        private State state;
+        private float timeSpentRolling;
 
-    private float currentRunSpeed;
-    private float currentRollSpeed;
-    private float maxSpeed;
-    private float defaultRunAnimation;
-
-    float turnSmoothVelocity;
-    float speedSmoothVelocity;
-    float currentSpeed;
-    Transform cameraT;
-    Animator animator;
-
-    void Start () {
-        state = State.Normal;
-        animator = GetComponent<Animator> ();
-
-        cameraT = Camera.main.transform;
-        defaultRunAnimation = runningAnimationSpeed;
-        maxSpeed = runSpeed * bonusSpeedMultiplier;
-        animator.SetFloat ("rollSpeed", rollingAnimationSpeed);
-    }
-
-    void Update () {
-        switch (state) {
-            case State.Normal:
-                Movement ();
-                Roll ();
-                CheckGround ();
-                break;
-            case State.Rolling:
-                Rolling ();
-                break;
-            case State.Falling:
-                Falling ();
-                break;
-        }
-    }
-
-    private void Movement () {
-        CheckBonusSpeed ();
-        bool running = Input.GetKey (KeyCode.LeftShift);
-        Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
-        Vector2 inputDir = input.normalized;
-
-        if (inputDir != Vector2.zero) {
-            float targetRotation = Mathf.Atan2 (inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
-            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle (transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
-        }
-        float targetSpeed = ((running) ? currentRunSpeed : walkSpeed) * inputDir.magnitude;
-        currentSpeed = Mathf.SmoothDamp (currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
-
-        transform.Translate (transform.forward * currentSpeed * Time.deltaTime, Space.World);
-
-        float animationSpeed = ((running) ? runningAnimationSpeed : walkingAnimationSpeed) * inputDir.magnitude;
-        animator.SetFloat ("forwardSpeed", animationSpeed, speedSmoothTime, Time.deltaTime);
-    }
-
-    private void Roll () {
-        if (Input.GetButtonDown ("Jump")) {
-            currentRollSpeed = rollSpeed;
-            animator.SetTrigger ("roll");
-            state = State.Rolling;
-            timeSpentRolling = 0f;
-
-            // animator.SetFloat ("rollSpeed", rollSpeed);
-        }
-    }
-
-    private void Rolling () {
-        float delay;
-        if (animator.GetFloat ("forwardSpeed") > 1) { delay = 0; } else { delay = rollDelay; }
-
-        timeSpentRolling += Time.deltaTime;
-        if (timeSpentRolling > delay && timeSpentRolling < maxRollTime) {
-            transform.Translate ((transform.forward * Time.deltaTime * currentRunSpeed), Space.World);
+        private enum State {
+            Normal,
+            Rolling,
+            Falling,
         }
 
-        if (timeSpentRolling >= maxRollTime) {
-            animator.SetFloat ("forwardSpeed", .5f);
-        }
+        private float currentRunSpeed;
+        private float currentRollSpeed;
+        private float maxSpeed;
+        private float defaultRunAnimation;
+        private float distanceToGround;
 
-        if (timeSpentRolling >= rollTime) {
+        float turnSmoothVelocity;
+        float speedSmoothVelocity;
+        float currentSpeed;
+        bool isGrounded;
+        Transform cameraT;
+        Animator animator;
+        CharacterController controller;
+
+        void Start () {
             state = State.Normal;
-            Debug.Log (state.ToString ());
-        }
-    }
+            animator = GetComponent<Animator> ();
 
-    private void CheckBonusSpeed () {
-        if (hasSpeedBonus) {
-            currentRunSpeed = maxSpeed;
-            runningAnimationSpeed = maxAnimationSpeed;
-            return;
+            cameraT = Camera.main.transform;
+            defaultRunAnimation = runningAnimationSpeed;
+            maxSpeed = runSpeed * bonusSpeedMultiplier;
+            animator.SetFloat ("rollSpeed", rollingAnimationSpeed);
+
+            distanceToGround = GetComponent<SphereCollider> ().bounds.extents.y;
         }
-        currentRunSpeed = runSpeed;
-        runningAnimationSpeed = defaultRunAnimation;
+
+        void Update () {
+            switch (state) {
+                case State.Normal:
+                    Movement ();
+                    Roll ();
+                    CheckGround ();
+                    break;
+                case State.Rolling:
+                    Rolling ();
+                    break;
+                case State.Falling:
+                    Falling ();
+                    CheckGround ();
+                    break;
+            }
+            controller = GetComponent<CharacterController> ();
+        }
+
+        private void Movement () {
+            CheckBonusSpeed ();
+            bool running = Input.GetKey (KeyCode.LeftShift);
+            Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+            Vector2 inputDir = input.normalized;
+
+            if (inputDir != Vector2.zero) {
+                float targetRotation = Mathf.Atan2 (inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+                transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle (transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+            }
+            float targetSpeed = ((running) ? currentRunSpeed : walkSpeed) * inputDir.magnitude;
+            currentSpeed = Mathf.SmoothDamp (currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+            transform.Translate (transform.forward * currentSpeed * Time.deltaTime, Space.World);
+
+            float animationSpeed = ((running) ? runningAnimationSpeed : walkingAnimationSpeed) * inputDir.magnitude;
+            animator.SetFloat ("forwardSpeed", animationSpeed, speedSmoothTime, Time.deltaTime);
+        }
+
+        private void Roll () {
+            if (Input.GetButtonDown ("Jump")) {
+                currentRollSpeed = rollSpeed;
+                animator.SetTrigger ("roll");
+                state = State.Rolling;
+                timeSpentRolling = 0f;
+
+                // animator.SetFloat ("rollSpeed", rollSpeed);
+            }
+        }
+
+        private void Rolling () {
+            float delay;
+            if (animator.GetFloat ("forwardSpeed") > 1) { delay = 0; } else { delay = rollDelay; }
+
+            timeSpentRolling += Time.deltaTime;
+            if (timeSpentRolling > delay && timeSpentRolling < maxRollTime) {
+                transform.Translate ((transform.forward * Time.deltaTime * currentRunSpeed), Space.World);
+            }
+
+            if (timeSpentRolling >= maxRollTime) {
+                animator.SetFloat ("forwardSpeed", .5f);
+            }
+
+            if (timeSpentRolling >= rollTime) {
+                state = State.Normal;
+                Debug.Log (state.ToString ());
+            }
+        }
+
+        private void Falling () {
+            transform.Translate (transform.forward * currentSpeed * Time.deltaTime, Space.World);
+        }
+
+        private void CheckGround () {
+            isGrounded = Physics.Raycast (transform.position + (Vector3.up * .2f) + (Vector3.forward * .1f), Vector3.down, distanceToGround + 0.5f) ||
+                Physics.Raycast (transform.position + (Vector3.up * .2f) + (Vector3.forward * -.1f), Vector3.down, distanceToGround + 0.5f) ||
+                Physics.Raycast (transform.position + (Vector3.up * .2f), Vector3.down, distanceToGround + 0.5f);
+
+            animator.SetBool ("isGrounded", isGrounded);
+            print (isGrounded);
+            if (isGrounded) {
+                state = State.Normal;
+                return;
+            }
+            state = State.Falling;
+        }
+
+        private void CheckBonusSpeed () {
+            if (hasSpeedBonus) {
+                currentRunSpeed = maxSpeed;
+                runningAnimationSpeed = maxAnimationSpeed;
+                return;
+            }
+            currentRunSpeed = runSpeed;
+            runningAnimationSpeed = defaultRunAnimation;
+        }
     }
 }
---
+++ Assets/TestController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour {
+    CharacterController controller;
+    // Start is called before the first frame update
+    [SerializeField] float speed = 6.0F;
+    [SerializeField] float jumpSpeed = 8.0F;
+    [SerializeField] float gravity = 20.0F;
+    [SerializeField] float turnSmoothTime = 0.2f;
+    float turnSmoothVelocity;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private float targetSpeed;
+    private bool running;
+
+    Transform cameraT;
+
+    void Start () {
+        controller = GetComponent<CharacterController> ();
+        cameraT = Camera.main.transform;
+    }
+
+    void Update () {
+        Movement ();
+    }
+
+    private void Movement () {
+        if (controller.isGrounded) {
+            running = Input.GetKey (KeyCode.LeftShift);
+            if (running) { targetSpeed = speed * 1.5f; } else { targetSpeed = speed; }
+
+            moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+            moveDirection = transform.TransformDirection (moveDirection);
+            moveDirection *= targetSpeed;
+
+            // if (moveDirection != Vector3.zero) {
+            //     float targetRotation = Mathf.Atan2 (moveDirection.x, moveDirection.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            //     transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle (transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+            // }
+
+            if (Input.GetButton ("Jump")) {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move (moveDirection * Time.deltaTime);
+    }
+}
